c5b48e2c936091a683f15722933dc3e2d3697558
ggusyyy <ggusy05128@gmail.com>
2025-07-07 18:20:30 +0200

    added BookAlreadyExists exception to CreateBookUseCase.

--- application/use_cases/create_book.py
+++ application/use_cases/create_book.py
@@ -1,4 +1,5 @@
 from application.dtos.create_book import CreateBookDTO
+from domain.exceptions.book_already_exists import BookAlreadyExists
 from domain.models.book import Book
 
 from domain.repositories.book_repository import BookRepository
@@ -9,6 +10,9 @@ class CreateBookUseCase:
         self.__book_repository: BookRepository = book_repository
     
     def run(self, input: CreateBookDTO) -> Book:
+        if self.__book_repository.get_by_id(input.id):
+            raise BookAlreadyExists()
+        
         book: Book = Book(input.id, input.title, input.author, input.availability)
         self.__book_repository.save(book)
         return book
