157d9723c11371c25e970ed3e7ba0311dbdc54fa
xsamueljr <samueljiro.alfa@gmail.com>
2025-07-09 00:07:28 +0200

    chore: better cursor handling in sqlite user repository

--- infrastructure/repositories/user/sqlite_user_repository.py
+++ infrastructure/repositories/user/sqlite_user_repository.py
@@ -10,47 +10,45 @@ from infrastructure.repositories.user.queries import UserQueries as UQ
 class SQLiteuserRepository(UserRepository):
     def __init__(self, db_path: str = "users.db") -> None:
         self.__conn: Connection = sqlite3.connect(db_path)
-        self.__cursor: Cursor = self.__conn.cursor()
+        cursor: Cursor = self.__conn.cursor()
         
-        self.__cursor.execute(UQ.CREATE_TABLE.value)
+        cursor.execute(UQ.CREATE_TABLE.value)
         self.__conn.commit()
-
-
-    def commit_and_close(self) -> None:
-        self.__conn.commit()
-        self.__cursor.close()
-  
+        cursor.close()
   
     def is_table_empty(self) -> bool:
-        self.__cursor.execute("SELECT * FROM users")
-        return self.__cursor.fetchone() is None
+        cursor: Cursor = self.__conn.execute("SELECT * FROM users")
+        is_empty = cursor.fetchone() is None
+        cursor.close()
+        return is_empty
     
     
     def save(self, user: User) -> None:
         if self.get_by_id(user.id) is None: 
-            self.__cursor.execute(
+            self.__conn.execute(
                 UQ.INSERT.value,
                 (user.id, user.name, user.reputation)
                 )
+            self.__conn.commit()
         else:
             self.update(user)
-        self.commit_and_close()
 
     
     def get_all(self) -> List[User]:
-        self.__cursor.execute(UQ.GET_ALL.value)
-        rows: List[Any] = self.__cursor.fetchall()
+        cursor: Cursor = self.__conn.execute(UQ.GET_ALL.value)
+        rows: List[Any] = cursor.fetchall()
+        cursor.close()
         users: List[User] = [
             User(id=row[0], name=row[1], reputation=row[2])
             for row in rows
             ]
-        self.commit_and_close()
         return users
     
     
     def get_by_id(self, id: str) -> Optional[User]:
-        self.__cursor.execute(UQ.GET_BY_ID.value, (id,))
-        row = self.__cursor.fetchone()
+        cursor: Cursor = self.__conn.execute(UQ.GET_BY_ID.value, (id,))
+        row = cursor.fetchone()
+        cursor.close()
         if row:
             return User(id=row[0], name=row[1], reputation=row[2])
         self.__conn.commit()
@@ -58,13 +56,13 @@ class SQLiteuserRepository(UserRepository):
     
     
     def update(self, user: User) -> None:
-        self.__cursor.execute(
+        self.__conn.execute(
             UQ.UPDATE.value,
             (user.name, user.reputation, user.id)
             )
-        self.commit_and_close()
+        self.__conn.commit()
     
     
     def delete(self, id: str) -> None:
-        self.__cursor.execute(UQ.DELETE.value, (id,))
-        self.commit_and_close()
+        self.__conn.execute(UQ.DELETE.value, (id,))
+        self.__conn.commit()
